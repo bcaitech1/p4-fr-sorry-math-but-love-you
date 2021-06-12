f4267f881748d472628287e201794daa3b155c86
ahaampo5 <ahaampo5@gmail.com>
2021-06-13 08:50:59 +0900

    Update soft_voting

--- utils.py
+++ utils.py
@@ -7,49 +7,50 @@ import torch
 import torch.optim as optim
 from networks.Attention import Attention
 from networks.SATRN import SATRN
-from networks.SWIN import SWIN
-# from networks.My_SATRN import MySATRN, MySATRN_de, MySATRN_en
-from networks.My_SATRN_v0 import MySATRN, MySATRN_de, MySATRN_en # NOTE: TransformerDecoderLayer 수정 전
-from networks.ASTER import ASTER
-
+from networks.My_SATRN import MySATRN, MySATRN_encoder, MySATRN_decoder
+from networks.SWIN import SWIN, SWIN_encoder, SWIN_decoder
+# from networks.ASTER import ASTER, ASTER_encoder, ASTER_decoder
 import warnings
-
 
 def get_network(
     model_type,
     FLAGS,
     model_checkpoint,
     device,
-    dataset,
-    decoding_manager=None,  # NOTE. available for ASTER, MySATRN, SWIN
+    train_dataset,
 ):
+    model = None
     if model_type == "Attention":
-        model = Attention(FLAGS, dataset, model_checkpoint).to(device)
-    elif model_type == "SATRN":
-        model = SATRN(FLAGS, dataset, model_checkpoint).to(device)
-    elif model_type == "SWIN":
-        pretrained_path = "/opt/ml/p4-fr-sorry-math-but-love-you_sub/pth/swin_tiny_patch4_window7_224.pth"
-        assert os.path.isfile(pretrained_path), f"There's no {pretrained_path}."
-        model = SWIN(FLAGS, dataset, model_checkpoint, decoding_manager).to(device)
-        checkpoint = torch.load(pretrained_path, map_location="cuda")
-        model.encoder.load_state_dict(checkpoint["model"], strict=False)
-    elif model_type == "MySATRN":
-        model = MySATRN(FLAGS, dataset, model_checkpoint, decoding_manager).to(device)
-    elif model_type == "ASTER":
-        model = ASTER(FLAGS, dataset, model_checkpoint, decoding_manager).to(device)
-    elif model_type == 'MySATRN_en':
-        model = MySATRN_en(FLAGS, dataset, model_checkpoint).to(device)
-    elif model_type == 'MySATRN_de':
-        model = MySATRN_de(FLAGS, dataset, model_checkpoint).to(device)
+        model = Attention(FLAGS, train_dataset, model_checkpoint).to(device)
+    elif model_type == 'SATRN':
+        model = SATRN(FLAGS, train_dataset, model_checkpoint).to(device)
+    elif model_type == 'MySATRN':
+        model = MySATRN(FLAGS, train_dataset, model_checkpoint).to(device)
+    elif model_type == 'MySATRN_encoder':
+        model = MySATRN_encoder(FLAGS, train_dataset, model_checkpoint).to(device)
+    elif model_type == 'MySATRN_decoder':
+        model = MySATRN_decoder(FLAGS, train_dataset, model_checkpoint).to(device)
+    elif model_type == 'SWIN':
+        model = SWIN(FLAGS, train_dataset, model_checkpoint).to(device)
+    elif model_type == 'SWIN_encoder':
+        model = SWIN_encoder(FLAGS, train_dataset, model_checkpoint).to(device)
+    elif model_type == 'SWIN_decoder':
+        model = SWIN_decoder(FLAGS, train_dataset, model_checkpoint).to(device)
+    # elif model_type == 'ASTER':
+    #     model = ASTER(FLAGS, train_dataset, model_checkpoint).to(device)
+    # elif model_type == 'ASTER_encoder':
+    #     model = ASTER_encoder(FLAGS, train_dataset, model_checkpoint).to(device)
+    # elif model_type == 'ASTER_decoder':
+    #     model = ASTER_decoder(FLAGS, train_dataset, model_checkpoint).to(device)
     else:
         raise NotImplementedError
 
     return model
 
-
 def get_optimizer(optimizer, params, lr, weight_decay=None):
     if optimizer == "Adam":
         optimizer = optim.Adam(params, lr=lr)
+
     elif optimizer == "Adadelta":
         optimizer = optim.Adadelta(params, lr=lr, weight_decay=weight_decay)
     elif optimizer == "AdamW":
@@ -57,7 +58,6 @@ def get_optimizer(optimizer, params, lr, weight_decay=None):
     else:
         raise NotImplementedError
     return optimizer
-
 
 def print_system_envs():
     num_gpus = torch.cuda.device_count()
@@ -70,19 +70,17 @@ def print_system_envs():
         "Memory Size : {}G\n".format(mem_size),
     )
 
-
 # Fixed version of id_to_string
 def id_to_string(tokens, data_loader, do_eval=0):
     result = []
     if do_eval:
-        eos_id = data_loader.dataset.token_to_id["<EOS>"]
-        special_ids = set(
-            [
-                data_loader.dataset.token_to_id["<PAD>"],
-                data_loader.dataset.token_to_id["<SOS>"],
-                eos_id,
-            ]
-        )
+        eos_id = data_loader.dataset.token_to_id['<EOS>']
+        special_ids = set([
+            data_loader.dataset.token_to_id['<PAD>'],
+            data_loader.dataset.token_to_id["<SOS>"],
+            eos_id
+            ])
+        
     for example in tokens:
         string = ""
         if do_eval:
@@ -98,18 +96,44 @@ def id_to_string(tokens, data_loader, do_eval=0):
                 token = token.item()
                 if token != -1:
                     string += data_loader.dataset.id_to_token[token] + " "
-
+        
         result.append(string)
     return result
 
+# Old: deprecated
+# def id_to_string(tokens, data_loader,do_eval=0):
+#     result = []
+#     if do_eval:
+#         special_ids = [
+#             data_loader.dataset.token_to_id["<PAD>"], 
+#             data_loader.dataset.token_to_id["<SOS>"],
+#             data_loader.dataset.token_to_id["<EOS>"]
+#             ]
 
-def set_seed(seed: int = 21):
+#     for example in tokens:
+#         string = ""
+#         if do_eval:
+#             for token in example:
+#                 token = token.item()
+#                 if token not in special_ids:
+#                     if token != -1:
+#                         string += data_loader.dataset.id_to_token[token] + " "
+#         else:
+#             for token in example:
+#                 token = token.item()
+#                 if token != -1:
+#                     string += data_loader.dataset.id_to_token[token] + " "
+
+#         result.append(string)
+#     return result
+
+
+def set_seed(seed: int=21):
     torch.manual_seed(seed)
     np.random.seed(seed)
     random.seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
 
-
 def get_timestamp():
-    return datetime.now().strftime(format="%m%d-%H%M%S")
+    return datetime.now().strftime(format='%m%d-%H%M%S')
