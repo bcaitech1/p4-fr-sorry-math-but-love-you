6b5e7f5d0798caaef06e78278b9a75fbba74a972
iloveslowfood <silkstaff@naver.com>
2021-06-17 05:00:05 +0000

    add annotations

--- utils/metrics.py
+++ utils/metrics.py
@@ -6,7 +6,12 @@ def final_metric(sentence_acc: float, word_error_rate: float) -> float:
     return 0.9 * sentence_acc + 0.1 * (1 - word_error_rate)
 
 
-def word_error_rate(predicted_outputs, ground_truths):
+def word_error_rate(predicted_outputs: list, ground_truths: list) -> float:
+    """Word Error Rate(WER)을 계산하는 함수
+
+    Reference.
+        - Wikipedia, 'Word error rate', https://en.wikipedia.org/wiki/Word_error_rate
+    """
     sum_wer = 0.0
     for output, ground_truth in zip(predicted_outputs, ground_truths):
         output = output.split(" ")
@@ -17,7 +22,11 @@ def word_error_rate(predicted_outputs, ground_truths):
     return sum_wer / len(predicted_outputs)
 
 
-def sentence_acc(predicted_outputs, ground_truths):
+def sentence_acc(predicted_outputs: list, ground_truths: list) -> float:
+    """Sentence Accuracy를 계산하는 함수
+
+    Sentence Accuracy = { SUM { Indicator { predicted == ground truth } } / Total number of sentences }
+    """
     correct_sentences = 0
     for output, ground_truth in zip(predicted_outputs, ground_truths):
         if np.array_equal(output, ground_truth):
